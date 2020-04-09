864810f918f48b23102d5b8326bc81046c9a4d7b
Thomas Boittin <tboittin.pro@gmail.com>
2020-04-09 20:48:49 +0200

    labtest

---
+++ LabTest/build/precache-manifest.00e54b80f1cd3646072cd02248836dd1.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af01a52ad0f1f178fe13697fa39084fe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5e127fe1ab43b1599e60",
+    "url": "/static/css/main.e6ba6ddd.chunk.css"
+  },
+  {
+    "revision": "5051e6f6f5703012fb09",
+    "url": "/static/js/2.15b34365.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.15b34365.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e127fe1ab43b1599e60",
+    "url": "/static/js/main.a4d1137c.chunk.js"
+  },
+  {
+    "revision": "6928bb372055118edd19",
+    "url": "/static/js/runtime-main.e81888ef.js"
+  },
+  {
+    "revision": "a6f407e83666dba166dde65c981e99f2",
+    "url": "/static/media/IMI-icon-human.a6f407e8.svg"
+  },
+  {
+    "revision": "5c11d1467418df2866f35c470bb49c31",
+    "url": "/static/media/IMI-icon-information.5c11d146.svg"
+  },
+  {
+    "revision": "988780db4b09d97a05018928a86eb79d",
+    "url": "/static/media/IMI-icon-less.988780db.svg"
+  },
+  {
+    "revision": "f425c624d3ef749f5978aa6177552118",
+    "url": "/static/media/IMI-icon-machine.f425c624.svg"
+  },
+  {
+    "revision": "7b5b9f2bbe500c5748d4cdeff3d2c580",
+    "url": "/static/media/IMI-icon-more.7b5b9f2b.svg"
+  },
+  {
+    "revision": "eaca18092816fd01cae57c16e1ee7cb0",
+    "url": "/static/media/messagere.eaca1809.jpg"
+  },
+  {
+    "revision": "732c3d2172e0b61ec049d243cc9720a9",
+    "url": "/static/media/pic2.732c3d21.jpg"
+  },
+  {
+    "revision": "21078325980467ffe28f1545dc695459",
+    "url": "/static/media/pic3.21078325.jpg"
+  },
+  {
+    "revision": "bf7ff3b50560d8a752b77dbdb2402682",
+    "url": "/static/media/pic4.bf7ff3b5.jpg"
+  },
+  {
+    "revision": "e53788bb6273f4bc46874a087ea1cded",
+    "url": "/static/media/pic5.e53788bb.jpg"
+  },
+  {
+    "revision": "ab8d5bfd95d94d5c7b72c4f9263ff6d0",
+    "url": "/static/media/pic6.ab8d5bfd.jpg"
+  }
+]);
